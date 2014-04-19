a742b2cd62eb1c93cad13637bc23497354c347d4
AJ Norton <ajn123@vt.edu>
2014-04-19 13:52:52 -0400

    added case statement

--- Beginner/booleans.rb
+++ Beginner/booleans.rb
@@ -1,3 +1,17 @@
 
 
+=begin
+you can use &&, ||, and, or for controlling statements
+=end
 
+
+distance = 22
+
+case distance
+  when 23
+    puts "23 miles away"
+  when 13
+    puts "13 miles away"
+  else #default case.
+    puts "??? miles away"
+end
