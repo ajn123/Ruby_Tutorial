f6a052713dc64e2c05b474a06ff770f6d9192fd5
AJ Norton <ajn123@vt.edu>
2014-04-19 01:47:59 -0400

    update readme

--- Inheritance.rb
+++ Inheritance.rb
@@ -1,3 +1,24 @@
 class  Probe
 
+  def deploy
+    puts "deploying"
+  end
+
 end
+
+=begin
+You can inherit attributes and classes in ruby.  Single inheritance
+is only allowed.
+=end
+
+class MineralProbe < Probe
+
+  def deploy
+    puts "preparing sample chamber"
+    super() #Calls the parents version of deploy.
+  end
+
+end
+
+
+MineralProbe.new.deploy()
