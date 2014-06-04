865196103e3a6738198a6e94edf261edb712a82a
Alex Norton <ajn123@vt.edu>
2014-06-04 01:13:02 -0700

    added block and merge examples

--- block.rb
+++ block.rb
@@ -1,16 +1,22 @@
+=begin
+You can define blocks
+
+=end
 
 
 array = (1..5)
 
+#Returns an array of elements that reach the criteria.
 array.select{|i| i > 3}
 
 
-#
-puts array.inject{:+}
+#Sums the entire array.
 puts array.inject{|num,sum| num + sum}
 
 
 
 #Applies a rule to every element in the collection.
 puts array.map{|num| num += 2}
+
+
 
--- hashes.rb
+++ hashes.rb
@@ -8,53 +8,58 @@ A hash is a unordered, object indexed collection of objects.
 Symbols refer to the same address space in memory as long as they are named the
 same.
 =end
-people = {:firstName => 'aj',:lastName => 'Norton'}
+person = {:firstName => 'aj',:lastName => 'Norton'}
 
 
 
 #print all values.
-people.values.each do |value|
+person.values.each do |value|
   puts "value is #{value}"
 end
 
 #print all keys
-people.keys.each do |key|
+person.keys.each do |key|
   puts "key is #{key}"
 end
 
 
-puts people['firstName']
+puts person['firstName']
 
-puts people.key('aj')
+puts person.key('aj')
 
 #prints out all the keys for that hash
-puts people.keys
+puts person.keys
 
 #prints out all the values for that hash
-puts people.values
+puts person.values
 
 #Prints out how many key value pairs there are (2 in this case).‰
-puts people.length
+puts person.length
 
 
 
 #prints out the contents of each key value pair as the hash
 #is iterated over.
-people.each do |key, value|
+person.each do |key, value|
   print key,value, "\n"
 end
 
 
 #print out each entry in a hash in a different way
-people.each_entry do |n,i|
+person.each_entry do |n,i|
   print n," ", i,"\n"
 end
 
 
 #One line way to print out
-people.values.each {|item| puts item}
+person.values.each {|item| puts item}
 
 
 puts "One line example"
-people.each {|key,value| puts "#{key}, #{value}"}
+person.each {|key,value| puts "#{key}, #{value}"}
 
+
+hash = {key: "a",value:"b"}
+
+#Merges the two hashes together into ones.
+puts hash.merge({extra: "c"})
