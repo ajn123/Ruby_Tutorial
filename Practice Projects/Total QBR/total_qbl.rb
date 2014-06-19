991bfea9c511fd0985b7bd8ba0c2501e87051b3a
A.J. Norton <ajn123@vt.edu>
2014-06-18 21:34:47 -0700

    started correct coding guidlines in  total_qbr

--- Practice Projects/Total QBR/total_qbl.rb
+++ Practice Projects/Total QBR/total_qbl.rb
@@ -1,22 +1,21 @@
 require 'csv'
-=begin
-This simple program calculates the QBR of a quarterback from a .csv file
-=end
+
+# This simple program calculates the QBR of a quarterback from a .csv file
 
 # Reads in a csv file and calculates the QBR
 def read_csv(file_name)
-  CSV.foreach(file_name,headers: true) do |row|
-    print row['Year'.gsub('*',' ')], ' '
+  CSV.foreach(file_name, headers: true) do |row|
+    print row['Year'.gsub('*', ' ')], ' '
     qbr(row['Cmp'].to_f, row['Att'].to_f, row['TD'].to_f, row['Int'].to_f, row['Yds'].to_f())
   end
 end
 
-def comp_att_rating(comp,attempts)
+def comp_att_rating(comp, attempts)
   correct_value (((comp/attempts) * 100) - 30) / 20
 
 end
 
-def tds_per_attempt(td,att)
+def tds_per_attempt(td, att)
   correct_value (((td/att) * 100)/5)
 
 end
@@ -26,7 +25,7 @@ def ints_per_attempt(int,att)
 
 end
 
-def yards_per_attempt(yards,att)
+def yards_per_attempt(yards, att)
   correct_value ((yards/att) - 3) / 4
 
 end
@@ -34,14 +33,21 @@ end
 def correct_value(val)
   min = 0
   max = 2.375
-  return min if val < min
-  return max if val > max
-  return val
+
+  if val < min
+    min
+  elsif val > max
+    max
+  else
+    val
+  end
 end
 
-def qbr(comp,attempts,td,int,yards)
-  print 'total QBR: ',(comp_att_rating(comp,attempts) + tds_per_attempt(td,attempts) + ints_per_attempt(int,attempts) +
-    yards_per_attempt(yards,attempts)) / 0.06, "\n"
+def qbr(comp, attempts, td, int, yards)
+  print 'total QBR: ', (comp_att_rating(comp, attempts) + tds_per_attempt(td,attempts) +
+                        ints_per_attempt(int,attempts) +
+                        yards_per_attempt(yards, attempts)) / 0.06, "\n"
 end
 
 read_csv('data.csv')
+
