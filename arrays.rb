9451ab4530a716183ccd4be08eb08942e9df4d20
A.J. Norton <ajn123@vt.edu>
2014-06-19 23:37:47 -0700

    cleaned up arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -1,47 +1,36 @@
-
-#creates an array with 4 copies of mac.
-names = Array.new(4, "mac")
+# creates an array with 4 copies of mac.
+names = Array.new(4, 'mac')
 
 puts "#{names}"
 
+nums = [1, 2, 3, 5, 4]
 
-nums = [1, 2, 3, 5,4]
-
-
-puts nums.max #prints the max number in the array, which is 5.
+puts nums.max # prints the max number in the array, which is 5.
 puts nums.min
 
-puts nums[-4] #You can count backgrounds from the end of an array. Prints "2".
+puts nums[-4] # You can count backgrounds from the end of an array. Prints "2".
 
-
-#adds an element to the end of the array.
+# adds an element to the end of the array.
 nums << 23.4
 
 puts "#{nums}"
 
-
-
-
-#returns back the array in a string format so you can view it
+# returns back the array in a string format so you can view it
 puts names.inspect
 
+# sorts the array, permanately altering the array. [1, 2, 3, 4, 5, 23.4]
+print nums.sort!
 
-print nums.sort!  #sorts the array, permanately altering the array. [1, 2, 3, 4, 5, 23.4]
+# Perform operations over an entire list.
+puts nums.map { |item| item * 3 }
 
+# Selects only the even numbers in the array.
+puts nums.select { |item| item.even? == 0 }
 
-puts nums.map{|item| item*3}#perform operations over an entire list.
+puts [1, 2, 3, 44, 55, 66].delete_if { |i| i < 4 }
 
-
-puts nums.select{|item| item % 2 == 0} #selects only the even numbers in the array.
-
-
-puts [1,2,3,44,55,66].delete_if{|i| i < 4}
-
-array = [5,5,5,1]
-array.delete 5  #deletes all instances of 5 in an array.
+array = [5, 5, 5, 1]
+array.delete 5  # deletes all instances of 5 in an array.
 puts array.to_s
 
-
-
-puts [[1,2,3],[2,3,4]].flatten
-
+puts [[1, 2, 3], [2, 3, 4]].flatten
