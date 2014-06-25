92b49a68dab3996e9d1c3b1e01830cd5c6afa9e7
A.J. Norton <ajn123@vt.edu>
2014-06-25 01:26:09 -0700

    updated string example with comments

--- Beginner/strings.rb
+++ Beginner/strings.rb
@@ -4,6 +4,18 @@
 # formatting/injection.
 puts 'hello there' # => hello there
 
+# Escape sequences will be translated literally
+puts 'hello \n there'
+
 # Strings with double quotes may have ruby code inserted into them with
 # the user of #{YOUR_CODE_HERE}.
 puts "I am #{9+20} years old" # =>  I am 29 years old
+# You can insert Anything into a string interpolation.
+puts "I am #{def add(a, b)
+               a + b
+             end
+             add(7, 8)} years old."
+
+# Double quoted strings will interpret escape sequences.
+puts "hi there \n my name is AJ" # => hi there
+                                 #     my name is AJ
